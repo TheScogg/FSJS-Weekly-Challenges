6954f8a052e88f72e88b52c0063f1c3b55436d1d
TheScogg <thescogg@gmail.com>
2016-09-22 18:02:18 -0400

    Week 4 Challenge Completed

--- Challenges/Week4/ChallengeFiles/js/main.js
+++ Challenges/Week4/ChallengeFiles/js/main.js
@@ -1,19 +1,38 @@
 function openBlock (id) {
     var block = document.getElementById(id);
-    var blocks = document.getElementById("main_content").getElementsByTagName("div");
 
+    //var test = document.getElementById("about");
+
+
+    console.log(block);
+    var blocks = document.getElementById("main_content").getElementsByTagName("div");
     for (var y = 0; y < blocks.length; y++) {
         blocks[y].style.display = "none";
     }
     block.style.display = "block";
-    
+    //document.getElementById("about").style.display = "block";
+
+
+    $("menu").css("display", "block");
     // TODO: remove the "active" class from all of the li elements inside the menu
+    $("#menu").children("li").removeClass("active");
     // TODO: add the "active" class to the li element that contains the link that was clicked
+    $('#' + id).parent().addClass("active");
+
 
 }
 
 // TODO: add the "hover" class to the menu items when you hover over them
-
+$("#menu").find("a").hover(function () {
+    //console.log(this);
+    $(this).addClass("hover");
+    }, function () {
+    //console.log("out");
+    $(this).removeClass("hover");
+    });
 
 // TODO: set up the tooltip plugin on all of the links in the menu
+$('#menu a').tooltip({
+    "delay" : "1000"
+});
 
