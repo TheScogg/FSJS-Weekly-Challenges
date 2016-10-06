89343c3cd10fcf05239f610836b2865d3673fa8e
James Scoggins <thescogg@gmail.com>
2016-10-06 15:51:54 -0400

    Week 6 Code Challenge

--- Challenges/Week6/js/app.js
+++ Challenges/Week6/js/app.js
@@ -1,3 +1,60 @@
+$(document).ready(function () {
+
+    function Box(height, width, volume) {
+        this.height = height;
+        this.width = width;
+        this.volume = volume;
+    }
+
+    var myBox = new Box(0, 0, 0);
+
+    $("table button").on('click', function () {
+        console.log(this.id);
+        if (this.id === "HeightIncrease") {
+            myBox.height++;
+            $("#height").html("Height: \n" + myBox.height);
+        } else if (this.id === "HeightDecrease" && myBox.height > 0) {
+            myBox.height--;
+            $("#height").html("Height: \n" + myBox.height);
+        } else if (this.id === "WidthIncrease") {
+            myBox.width++;
+            $("#width").html("Width: \n" + myBox.width);
+        } else if (this.id === "WidthDecrease" && myBox.width > 0) {
+            myBox.width--;
+            $("#width").html("Width: \n" + myBox.width);
+        } else if (this.id === "VolumeIncrease") {
+            myBox.volume++;
+            $("#volume").html("Volume: \n" + myBox.volume);
+        } else if (this.id === "VolumeDecrease" && myBox.volume > 0) {
+            myBox.volume--;
+            $("#volume").html("Volume: \n" + myBox.volume);
+        } else if (this.id === "Print") {
+            $("#output").html("Height: " + myBox.height + "<br>" + "Width: " + myBox.width + "<br>" + "Volume: " + myBox.volume);
+        }
+    });
+
+
+    //$("#HeightDecrease").on('click', function () {
+    //
+    //});
+    //
+    //$("#WeightIncrease").on('click', function () {
+    //
+    //});
+    //
+    //$("#WeightDecrease").on('click', function () {
+    //
+    //});
+    //
+    //$("#VolumeIncrease").on('click', function () {
+    //    console.log("Height Increased");
+    //});
+
+
+});
+
+
+
 //#Week 6 JQuery Code Challenge
 
 //Objects
